527e5476adb734c907c9d15ecaeffead3c679ba8
matanLS <matan@lightsolver.com>
2024-03-14 10:26:44 +0200

    setup_env.py add support for mac

--- setup_env.py
+++ setup_env.py
@@ -1,5 +1,7 @@
 import getpass
+import platform
 import subprocess
+import os
 from laser_mind_client import LaserMind
 
 print("This is a setup script for the laser-mind-client")
@@ -17,8 +19,20 @@ except Exception as ex:
 print("Setting environment variables")
 
 try:
-    subprocess.call(['powershell.exe', '-Command', f'[Environment]::SetEnvironmentVariable("LS_USER", "{ls_user}", "User")'])
-    subprocess.call(['powershell.exe', '-Command', f'[Environment]::SetEnvironmentVariable("LS_PASS", "{ls_pass}", "User")'])
+    # Change temporarily so user doens't have to respawn the terminal
+    os.environ["LS_USER"] = ls_user
+    os.environ["LS_PASS"] = ls_pass
+
+    # Permanently set for future terminal sessions, per platform
+    if platform.system() == "Windows":
+        subprocess.call(['powershell.exe', '-Command', f'[Environment]::SetEnvironmentVariable("LS_USER", "{ls_user}", "User")'])
+        subprocess.call(['powershell.exe', '-Command', f'[Environment]::SetEnvironmentVariable("LS_PASS", "{ls_pass}", "User")'])
+    else:
+        profile_path = os.path.expanduser("~/.bash_profile")
+        with open(profile_path, 'a') as profile_file:
+            profile_file.write(f'\nexport LS_USER="{ls_user}"\n')
+            profile_file.write(f'export LS_PASS="{ls_pass}"\n')
+
 except Exception as ex:
     print("Error updating environment variables, please verify that you are running as administrator")
     print("Setup incomplete.")
